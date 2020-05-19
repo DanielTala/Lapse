48d1fc8744acff666831c4ae04c40cc7de110741
DanielTala <55824690+DanielTala@users.noreply.github.com>
2020-05-19 22:41:22 +0800

    Compilation OTW

---
+++ Assets/Portal.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private bool portal;
+
+    private void Update()
+    {
+        if (portal==true && Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene(3);
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            portal = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            portal = false;
+        }
+    }
+}
--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -2,24 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
 
     private float currentTime = 0f;
     public float startingTime = 60f;
-
+    private string scene;
+   
     [SerializeField] Text countdownText;
     // Start is called before the first frame update
     void Start()
     {
         currentTime = startingTime;
+        countdownText.text = currentTime.ToString("0");
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentTime -= 1 * Time.deltaTime;
-        countdownText.text = currentTime.ToString("0");
+       scene = SceneManager.GetActiveScene().name;
+        Debug.Log(scene);
+        if (scene == "Tilemap Combat")
+        {
+            currentTime -= 1 * Time.deltaTime;
+            countdownText.text = currentTime.ToString("0");
+        }
     }
+
+
 }
